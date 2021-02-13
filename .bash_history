d3c965a5a1cdfaae192a1d9c8be75fbabdbd4143
ravibarla <ravibarla1@gmail.com>
2021-02-13 21:24:09 +0530

    added cartIcon and cartDropdown components to our application, also implement cartReducer

--- .bash_history
+++ .bash_history
@@ -6,3 +6,22 @@ code .
 yarn start
 yarn start
 git status
+yarn start
+git status
+git add -A
+git commit -m"updating our app to redirect to home if user is signned in"
+git commit -m"updating our app to redirect to home if user is signned in"
+git config user.email"ravibarla1@gmail.com"
+git config user.name"ravi barla"
+git config user.name"ravibarla"
+git commit -m"updating our app to redirect to home if user is signned in"
+git config user.email"ravibarla1@gmail.com"
+git config user.name"ravibarla"
+git commit -m"updating our app to redirect to home if user is signned in"
+git config --global user.email "ravibarla1@gmail.com"
+git config --global user.name "ravibarla"
+git status
+git commit -m"updating our app to redirect to home if user is signned in"
+git push origin master
+yarn start
+view $home
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils.js";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -26,11 +28,14 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
